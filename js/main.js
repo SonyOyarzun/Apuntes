4c410ac487432c3501eb8e95765511642c61e543
Sony Oyarzun <Sony.oyarzun@gmail.com>
2019-01-31 00:47:48 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+function loadScript(url, callback){
+
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    script.onreadystatechange = callback;
+    script.onload = callback;
+    head.appendChild(script);
+}
+
+loadScript('js/pdf.js','pdf');
---
+++ js/pdf.js
@@ -0,0 +1,41 @@
+//http://rogerdudler.github.io/git-guide/index.es.html-->
+function appWeb(embed){
+   
+    if(embed==0){
+        document.getElementById("embed1").src="documentos/APLICACIONES_WEB/BackupBD.pdf";
+    }
+    if(embed==1){
+        document.getElementById("embed1").src="documentos/APLICACIONES_WEB/1.Crear_Form_Pagemaster.pdf";
+    }
+    if(embed==2){
+        document.getElementById("embed1").src="documentos/APLICACIONES_WEB/2.CrearTBLSQLserver2014.pdf";
+    }
+    if(embed==3){
+        document.getElementById("embed1").src="documentos/APLICACIONES_WEB/3.UnirSQL_VISUAL.pdf";
+    }
+}
+
+function herrS(embed){
+   
+    if(embed==0){
+        document.getElementById("embed1").src="documentos/HERRAMIENTAS_S.O/1.Ensamblado Prueba.pdf";
+    }
+    if(embed==1){
+        document.getElementById("embed1").src="documentos/HERRAMIENTAS_S.O/2.Actualizar debian.pdf";
+    }
+    if(embed==2){
+        document.getElementById("embed1").src="documentos/HERRAMIENTAS_S.O/3.X ming.pdf";
+    }
+    if(embed==3){
+        document.getElementById("embed1").src="documentos/HERRAMIENTAS_S.O/4.Apuntes Dominio Server.pdf";
+    }
+    if(embed==4){
+        document.getElementById("embed1").src="documentos/HERRAMIENTAS_S.O/5.Creacion estructura.pdf";
+    }
+    if(embed==5){
+        document.getElementById("embed1").src="documentos/HERRAMIENTAS_S.O/6.Crear Cliente FTP.pdf";
+    }
+    if(embed==6){
+        document.getElementById("embed1").src="documentos/HERRAMIENTAS_S.O/Samba.pdf";
+    }
+}
