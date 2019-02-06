492e48329d2de3d5ff131bdaec404d24651758ae
Sony Oyarzun <Sony.oyarzun@gmail.com>
2019-02-06 00:45:23 -0300

    Commit message

--- js/pdf.js
+++ js/pdf.js
@@ -125,3 +125,163 @@ function ingles(embed){
         document.getElementById("embed1").src="documentos/INGLES/VerbosIrregulares.pdf";
     }
 }
+
+
+//SONIDO
+
+function acondAcust(embed){
+   
+    if(embed==0){
+        document.getElementById("embed1").src="Documentos2/Acondicionamiento Acustico/01_Seleccion_Aislamiento.pdf";
+    }
+    if(embed==1){
+        document.getElementById("embed1").src="Documentos2/Acondicionamiento Acustico/02_Panel_Simple.pdf";
+    }
+    if(embed==2){
+        document.getElementById("embed1").src="Documentos2/Acondicionamiento Acustico/03_Panel_Doble.pdf";
+    }
+    if(embed==3){
+        document.getElementById("embed1").src="Documentos2/Acondicionamiento Acustico/04_Aislamiento_Normas.pdf";
+    }
+    if(embed==4){
+        document.getElementById("embed1").src="Documentos2/Acondicionamiento Acustico/05_Aislamiento_Configuraciones.pdf";
+    }
+    if(embed==5){
+        document.getElementById("embed1").src="Documentos2/Acondicionamiento Acustico/06_Diseno_Acustico_de_Recintos.pdf";
+    }
+    if(embed==6){
+        document.getElementById("embed1").src="Documentos2/Acondicionamiento Acustico/07_Diseno_de_Estudios_de_Grabacion.pdf";
+    }
+    if(embed==7){
+        document.getElementById("embed1").src="Documentos2/Acondicionamiento Acustico/08_Evaluacion_Acustica_de_Recintos.pdf";
+    }
+//Guias
+
+    if(embed==8){
+        document.getElementById("embed1").src="Documentos2/Acondicionamiento Acustico/Guias acondicionamiento/guia01_2011_aislamiento_panel_simple.pdf";
+    }
+    if(embed==9){
+        document.getElementById("embed1").src="Documentos2/Acondicionamiento Acustico/Guias acondicionamiento/guia02_2011_aislamiento_panel_doble.pdf";
+    }
+    if(embed==10){
+        document.getElementById("embed1").src="Documentos2/Acondicionamiento Acustico/Guias acondicionamiento/guia03_disenoacustico_2010.pdf";
+    }
+    if(embed==11){
+        document.getElementById("embed1").src="Documentos2/Acondicionamiento Acustico/Guias acondicionamiento/modos_normales.xls";
+    }
+}
+
+
+
+function disenoInstal(embed){
+   
+    if(embed==0){
+        document.getElementById("embed1").src="Documentos2/Diseño e Instalacion/Informe2 Cad-Catt.pdf";
+    }
+    if(embed==1){
+        document.getElementById("embed1").src="Documentos2/Diseño e Instalacion/Unidad_I_Parte_1_Microfonos_v3.pdf";
+    }
+    if(embed==2){
+        document.getElementById("embed1").src="Documentos2/Diseño e Instalacion/Unidad_I_Parte_2_Altavoces_v2.pdf";
+    }
+    if(embed==3){
+        document.getElementById("embed1").src="Documentos2/Diseño e Instalacion/Unidad_II_Parte_1_Preamplificadores_v2.pdf";
+    }
+    if(embed==4){
+        document.getElementById("embed1").src="Documentos2/Diseño e Instalacion/Unidad_II_Parte_2_Amplificadores_de_Potencia_v2.pdf";
+    }
+    if(embed==5){
+        document.getElementById("embed1").src="Documentos2/Diseño e Instalacion/Unidad_II_Parte_3_EQs_v2.pdf";
+    }
+    
+//Guias
+
+    if(embed==6){
+        document.getElementById("embed1").src="Documentos2/Diseño e Instalacion/Guias diseño/guia01_microfonos_altavoces_2010.pdf";
+    }
+    if(embed==7){
+        document.getElementById("embed1").src="Documentos2/Diseño e Instalacion/Guias diseño/guia02_procesadores_preamplificadores_2010.pdf";
+    }
+    if(embed==8){
+        document.getElementById("embed1").src="Documentos2/Diseño e Instalacion/Guias diseño/2011_guia03_gananciacustica.pdf";
+    }
+    if(embed==9){
+        document.getElementById("embed1").src="Documentos2/Diseño e Instalacion/Guias diseño/guia04_amplificadores_eqs_xovers_2010.pdf";
+    }
+}
+
+
+function post(embed){
+   
+    if(embed==0){
+        document.getElementById("embed1").src="Documentos2/Post produccion/historia Cine-Equipo tecnico.pdf";
+    }
+    if(embed==1){
+        document.getElementById("embed1").src="Documentos2/Post produccion/Pasos Post audio.pdf";
+    }
+    if(embed==2){
+        document.getElementById("embed1").src="Documentos2/Post produccion/Presentacion_Automatizacion.pdf";
+    }
+    if(embed==3){
+        document.getElementById("embed1").src="Documentos2/Post produccion/SEMPTE-Formatos Cine.pdf";
+    }
+}
+
+
+function recinto(embed){
+   
+    if(embed==0){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/dUnidad_I_Parte_1_Analogias_v2.pdf";
+    }
+    if(embed==1){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/dUnidad_I_Parte_2_Cto_Acustico_y_Electrico_v2.pdf";
+    }
+    if(embed==2){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/dUnidad_I_Parte_3_Parametros_TS_v2.pdf";
+    }
+    if(embed==3){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/dUnidad_I_Parte_4_Impedancia_Electrica_y_Funcion_de_Respuesta_v2.pdf";
+    }
+    if(embed==4){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/dUnidad_I_Parte_5_Trabajo_en_Alta_Potencia_v2.pdf";
+    }
+    if(embed==5){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/dUnidad_II_Parte_1_Caja_Cerrada_v2.pdf";
+    }
+    if(embed==6){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/dUnidad_II_Parte_2_Construccion_Gabinete_v2.pdf";
+    }
+    if(embed==7){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/dUnidad_II_Parte_3_Caja_Rellena_v3.pdf";
+    }
+    if(embed==8){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/INFORME 1 RECINTOS.pdf";
+    }
+    if(embed==9){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/Unidad_III_Parte_1_Alineamientos_Bass_Reflex_v3.pdf";
+    }
+    if(embed==10){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/Unidad_III_Parte_2_Ducto_Bass_Reflex_v2.pdf";
+    }
+    if(embed==11){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/Unidad_III_Parte_3_Formatos_de_Caja_con_Varios_Altavoces_v2.pdf";
+    }
+    if(embed==12){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/Xovers.pdf";
+    }
+    if(embed==13){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/Recinto para altavoces final.pdf";
+    }
+   
+//Guias
+
+    if(embed==14){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/Guias recinto/2011_guia01_altavoz_pantalla_infinita.pdf";
+    }
+    if(embed==15){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/Guias recinto/guia02_cb_2010.pdf";
+    }
+    if(embed==16){
+        document.getElementById("embed1").src="Documentos2/Recinto de Altavoces/Guias recinto/guia03_br.pdf";
+    }
+}
