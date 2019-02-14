cd94ab951195200d2cfa19e70a4e10b755902078
sony <sony.oyarzun@gmail.com>
2019-02-14 10:08:31 -0300

    Commit message

--- js/webcam.js
+++ js/webcam.js
@@ -1,97 +1,31 @@
-/*
-var video = document.querySelector("#video");
+     window.onload = function() {
+          var video = document.getElementById('video');
+          var canvas = document.getElementById('canvas');
+          var context = canvas.getContext('2d');
+          var tracker = new tracking.ObjectTracker('face');
+          tracker.setInitialScale(4);//4
+          tracker.setStepSize(2);
+          tracker.setEdgesDensity(0.1);
+          
+          tracking.track('#video', tracker, { camera: true });
 
-if (navigator.mediaDevices.getUserMedia) {       
-    navigator.mediaDevices.getUserMedia({video: true})
-  .then(function(stream) {
-    video.srcObject = stream;
-  })
-  .catch(function(err0r) {
-    console.log("Something went wrong!");
-  });
-}
-*/
+          tracker.on('track', function(event) {
+            //si encuentra cara...
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            event.data.forEach(function(rect) {
+              context.strokeStyle = '#a64ceb';
+              context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+              context.font = '11px Helvetica';
+              context.fillStyle = "#fff";
+              context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+              context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+            });
+          });
 
-$(document).ready(function() {
-    run()
-  })
+
+          var gui = new dat.GUI();
+          gui.add(tracker, 'edgesDensity', 0.1, 0.5).step(0.01);
+          gui.add(tracker, 'initialScale', 1.0, 10.0).step(0.1);
+          gui.add(tracker, 'stepSize', 1, 5).step(0.1);
+        };
       
-  async function run() {
-    // load the models
-    await faceapi.loadMtcnnModel('/')
-    await faceapi.loadFaceRecognitionModel('/')
-    
-    // try to access users webcam and stream the images
-    // to the video element
-    const videoEl = document.getElementById('Video')
-    navigator.getUserMedia(
-      { video: {} },
-      stream => videoEl.srcObject = stream,
-      err => console.error(err)
-    )
-  }
-
-/*
-
-
-(function() {
-
-  var streaming = false,
-      video        = document.querySelector('#video'),
-      canvas       = document.querySelector('#canvas'),
-      photo        = document.querySelector('#photo'),
-      startbutton  = document.querySelector('#startbutton'),
-      width = 320,
-      height = 0;
-
-  navigator.getMedia = ( navigator.getUserMedia ||
-                         navigator.webkitGetUserMedia ||
-                         navigator.mozGetUserMedia ||
-                         navigator.msGetUserMedia);
-
-  navigator.getMedia(
-    {
-      video: true,
-      audio: false
-    },
-    function(stream) {
-      if (navigator.mozGetUserMedia) {
-        video.mozSrcObject = stream;
-      } else {
-        var vendorURL = window.URL || window.webkitURL;
-        video.src = vendorURL.createObjectURL(stream);
-      }
-      video.play();
-    },
-    function(err) {
-      console.log("An error occured! " + err);
-    }
-  );
-
-  video.addEventListener('canplay', function(ev){
-    if (!streaming) {
-      height = video.videoHeight / (video.videoWidth/width);
-      video.setAttribute('width', width);
-      video.setAttribute('height', height);
-      canvas.setAttribute('width', width);
-      canvas.setAttribute('height', height);
-      streaming = true;
-    }
-  }, false);
-
-  function takepicture() {
-    canvas.width = width;
-    canvas.height = height;
-    canvas.getContext('2d').drawImage(video, 0, 0, width, height);
-    var data = canvas.toDataURL('image/png');
-    photo.setAttribute('src', data);
-  }
-
-  startbutton.addEventListener('click', function(ev){
-      takepicture();
-    ev.preventDefault();
-  }, false);
-
-})();
-
-*/
