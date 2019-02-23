41f2e907426986cdbf1e8afc402d7bda287e3ee5
sony <sony.oyarzun@gmail.com>
2019-02-23 15:15:00 -0300

    Commit message

--- js/webcam.js
+++ js/webcam.js
@@ -1,6 +1,5 @@
-    
-    
-    window.onload = function() {
+
+window.onload = function() {
 
           var video = document.getElementById('video');
           var canvas = document.getElementById('canvas');
@@ -24,44 +23,15 @@
               context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
               context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
 
-              ////////////////////////////
+            ////////////////////////////
 
               capturar(canvas,context);
 
-              enviar2(foto);
-             
+              enviar2();
 
-            
-				//Pausar reproducción
-			/*	video.pause();
+         
 
-				//Obtener contexto del canvas y dibujar sobre él
-				var contexto = canvas.getContext("2d");
-				canvas.width = video.videoWidth;
-				canvas.height = video.videoHeight;
-				contexto.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-				var foto = canvas.toDataURL(); //Esta es la foto, en base 64
-				estado.innerHTML = "Enviando foto. Por favor, espera...";
-				var xhr = new XMLHttpRequest();
-				xhr.open("POST", "./guardar_foto.php", true);
-				xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-				xhr.send(encodeURIComponent(foto)); //Codificar y enviar
-
-				xhr.onreadystatechange = function() {
-				    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-				        console.log("La foto fue enviada correctamente");
-				        console.log(xhr);
-				        estado.innerHTML = "Foto guardada con éxito. Puedes verla <a target='_blank' href='./" + xhr.responseText + "'> aquí</a>";
-				    }
-				}
-
-				//Reanudar reproducción
-				video.play();
-*/
-
-
-              /////////////////////////////////////////////////
+            /////////////////////////////////
             });
           });
 
@@ -70,8 +40,12 @@
           gui.add(tracker, 'edgesDensity', 0.1, 0.5).step(0.01);
           gui.add(tracker, 'initialScale', 1.0, 10.0).step(0.1);
           gui.add(tracker, 'stepSize', 1, 5).step(0.1);
-        };
+};
       
+    
+
+
+
 function capturar(canvas,context){
 //  video.pause();
   canvas.width = video.videoWidth;
@@ -80,68 +54,63 @@ function capturar(canvas,context){
   var foto = canvas.toDataURL(); //Esta es la foto, en base 64
   document.getElementById("foto").src = foto;
   // alert("Imagen capturada");
+
+
 }
         
 function enviar(foto){
   
-  
+ /* 
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", "php/guardar_foto.php", true);
 	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xhr.send(encodeURIComponent(foto)); //Codificar y enviar
-  alert("Imagen enviada");
-  /*
-
- //var parametros = {
- // id : jQuery('#idUsuario').val(),
- // nombre : jQuery('#nombreUsuario').val(),
- // imagen : jQuery('#temporal').attr('src')
-//};
-
-
-
- $.ajax('php/guardar_foto.php', {
-  type : 'post',
-  //data : parametros,
-  data : foto,
-  success : function(data) {
-    alert("enviando1");
-    if(data.error){
-      console.log('Error controlado.', data.mensaje);
-      return;
-    };
-    console.log('Imagen ' + foto.imagen + ' fue guardada.');
-    alert('Imagen ' + foto.imagen + ' fue guardada.');
-  },
-  error : function(data) {
-    console.log('Error no controlado.', data);
-    alert('Error no controlado.', data);
-  }
-  
-});
-alert("enviando2");
-
-
 */
+
 }
 
-function enviar2(foto){
-  $.ajax({
-    type: 'POST',
+
+function enviar2(){
+
+  // si usamos el metodo POST provoca error 405 Method Not Allowed asi usaremos GET
+  // fuente https://airbrake.io/blog/http-errors/405-method-not-allowed
+
+  var request = $.ajax({
+    type: 'Get',
     url: 'php/guardar_foto.php',
     data: {
-            imagen:foto
-          },
-    contentType: false,
-    cache: false,
-    processData:false,
-    success: function(msg){
-       alert(msg);
-    }
-    
+        imagen: foto
+      },
+      contentType: false,
+      cache: false,
+      processData:false,
+    success: function(msg){ 
+     console.log(msg);
+    }  
+});
+
+request.done(function( data ) {
+  alert("Todo bien");
+  console.log(data); //Si pones el content-type en PHP no necesitas parse         
+});
+
+request.fail(function( jqXHR, textStatus ) {
+alert( "Hubo un error: " + textStatus );
 });
 
 
+}
 
 
+function enviar3(){
+
+  $.ajax({
+    url: 'php/guardar_foto.php',
+    success: function(respuesta) {
+      console.log(respuesta);
+    },
+    error: function() {
+          console.log("No se ha podido obtener la información");
+      }
+  });
 }
--- php/guardar_foto.php
+++ php/guardar_foto.php
@@ -1,65 +1,34 @@
-<?php
-$_POST['imagen']
+<?php 
 
-echo "hola";
 
-/*
-    protected $conexion;
-    protected $db;
-    
+protected $conexion;
+protected $db;
+
 // datos de Host
-    protected $HOST='localhost'; 
-    protected $USER='root';
-    protected $PASS='';
-    protected $DBNAME='captura';
+protected $HOST='localhost'; 
+protected $USER='root';
+protected $PASS='1234';
 
-    public function conectar()
-    {
-        $this->conexion = mysqli_connect($this->HOST, $this->USER, $this->PASS);
-        if ($this->conexion === 0) DIE("Lo sentimos, no se ha podido conectar con MySQL: " . mysqli_error());
-        $this->db = mysqli_select_db($this->conexion,$this->DBNAME);
-        if ($this->db == 0) DIE("Lo sentimos, no se ha podido conectar con la base datos: " . DBNAME);
-
-        return true;
-
-    }
-
-    public function desconectar()
-    {
-        if ($this->conectar()) {
-            mysqli_close($this->conexion);
-            echo '<br> FIN';
-        }
-    }
+protected $DBNAME='captura';
 
 
-    
+$this->conexion = mysqli_connect($this->HOST, $this->USER, $this->PASS);
+
+$sql= "INSERT INTO `prueba`(`nombre`) VALUES ('22')";
+
+$query = mysqli_query($this->conexion,$sql);
 
 
-    
-   
-      $codigo="09";
+  if ($query === false){ 
+      echo "<br>Sentencia incorrecta llamado a tabla";
+  }
+  else {
+      if(mysqli_affected_rows($this->conexion)==1){
+      echo '<br>Registro Exitoso';
+      } else {
+        echo '<br>error al insertar'; 
+      }
+  }
+
   
-
-        $this->conectar();
-        $tabla = "prueba";
-        $sql="INSERT INTO $tabla VALUES ('$codigo')";
-        $query = mysqli_query($this->conexion,$sql);
-
-        if ($query === false){ 
-            echo "<br>Sentencia incorrecta llamado a tabla: $tabla.";
-        }
-        else {
-            if(mysqli_affected_rows($this->conexion)==1){
-            echo '<br>Registro Exitoso';
-            } else {
-              echo '<br>error al insertar'; 
-            }
-        }
-        $this->desconectar();
-      
-
-        */
-
-
-
+  mysqli_close($this->conexion);
